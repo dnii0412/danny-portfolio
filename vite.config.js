3dbaf88c52e7dc72906eda3fd197a2d27a223e9c
Danny <dnioko0412@gmail.com>
2024-07-20 21:05:48 +0800

    Prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-portfolio/', // Replace with your repository name,
   server: {
     port: 3000,
   },
