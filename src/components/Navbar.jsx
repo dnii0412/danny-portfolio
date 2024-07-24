e847c02b8329ecf5798aad2b9029e106685b6e73
Danny <dnioko0412@gmail.com>
2024-07-24 15:33:28 +0800

    added navbar actions

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import '../index.css'
+import Hero from '../components/Hero'
+import Stuff from '../components/Stuffs'
+import Experience from '../components/Experience'
+import Skills from '../components/Skill'
+import Contact from '../components/Contact'
 const Navbar = () => {
-  // function clickProjects(){
+  // function cackProjects(){
   //   location.href("")
   // }
+
+  function homeClick(){
+
+  };
+  function expClick(){
+  };
+  function contactClick(){
+  };
+
   return (
     <>
       <header className='top-8 w-full fixed h-12 flex items-center justify-center sm-280:opacity-0 sm-280:-z-40 md:opacity-100 md:z-40'>
         <nav className='w-72 bg-white flex items-center justify-center h-full rounded-full drop-shadow-lg font-bold'>
           <ul className='border-borderColor flex w-full justify-around text-black text-lg z-50 relative'>
-            <li className='transition duration-200 hover:text-brown cursor-pointer' aria-label="Projects">
-              Projects
+            <li className='transition du`ration-200 hover:text-brown cursor-pointer' aria-label="Projects">
+            <Link to="/">Home</Link>
             </li>
             <li className='transition duration-200 hover:text-brown cursor-pointer' aria-label="Skills">
-              Skills
+              <Link to="/Stuff">Stuffs</Link>
             </li>
             <li className='transition duration-200 hover:text-brown cursor-pointer' aria-label="Contact">
-              Contact
+              <Link to="/Stuff">Skills</Link>
             </li>
           </ul>
         </nav>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-portfolio/', // Replace with your repository name,
+  base: '/react-portfolio/', // Ensure this matches your GitHub repository name
   server: {
     port: 3000,
   },
+  build: {
+    outDir: 'dist'
+  }
 })
